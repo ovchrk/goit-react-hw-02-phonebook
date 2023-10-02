633c229fd155bd7ef4345681e5a32f780c66f35c
ovchrk <113932934+ovchrk@users.noreply.github.com>
2023-10-02 22:15:46 +0300

    edits on gender

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,24 +7,25 @@ import { List } from './List';
 
 class App extends Component {
   state = {
-    contacts: [ { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
+    contacts: [ { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56', gender: 'female' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12', gender: 'female' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79', gender: 'male' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26', gender: 'female' },],
     filter: "",
   }
   
-  addContact = ({name, number}) => {
+  addContact = ({name, number, gender}) => {
     const newContact = {
       id: nanoid(),
       name: name,
       number: number,
+      gender: gender,
     }
     
     const inList = this.state.contacts.some(contact => contact.name === newContact.name);
 
     if (inList) {
-      alert(`${newContact.name} is already in your list`);
+      alert(`${newContact.name}  is already in contacts.`);
       return; 
     }
     this.setState(prevState => ({
